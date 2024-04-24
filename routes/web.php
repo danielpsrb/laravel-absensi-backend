fb70423fd27b94a7d9b8c0aa4b9577aff42a2d45
danielpsrb <danielpsrb01@gmail.com>
2024-04-24 17:27:40 +0700

    update route for dashboard

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('pages.dashboard');
+    return view('pages.dashboard', ['type_menu' => 'dashboard']);
 });
