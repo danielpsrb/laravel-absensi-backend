8b493b84db8de397331cf94a4efadc394e42c749
danielpsrb <danielpsrb01@gmail.com>
2024-04-24 18:25:23 +0700

    update fortify service for login

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -42,5 +42,9 @@ class FortifyServiceProvider extends ServiceProvider
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
         });
+
+        Fortify::loginView(function () {
+            return view('pages.auth.auth-login');
+        });
     }
 }
