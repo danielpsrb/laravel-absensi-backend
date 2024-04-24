db8abce593b889ab90dbe7ada2b17b82013bd321
danielpsrb <danielpsrb01@gmail.com>
2024-04-24 22:10:42 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.auth-login');
 });
 
 Route::get('/auth-forgot-password', function () {
