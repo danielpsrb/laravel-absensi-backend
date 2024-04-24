0529b1a7509a0073136e5a090cd5f8287e2589ea
danielpsrb <danielpsrb01@gmail.com>
2024-04-24 17:18:11 +0700

    add route for dashbooard

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('pages.dashboard');
+});
