469becd1e9b2f542c2cf16269092bd5f326daf06
danielpsrb <danielpsrb01@gmail.com>
2024-04-24 22:03:28 +0700

    make function logout

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -263,9 +263,13 @@
                 </a>
                 <div class="dropdown-divider"></div>
                 <a href="#"
-                    class="dropdown-item has-icon text-danger">
+                    class="dropdown-item has-icon text-danger"
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit()">
                     <i class="fas fa-sign-out-alt"></i> Logout
                 </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
             </div>
         </li>
     </ul>
