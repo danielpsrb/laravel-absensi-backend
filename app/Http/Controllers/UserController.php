72edc672715646ad987d714b1241f01b7129146f
danielpsrb <danielpsrb01@gmail.com>
2024-04-27 21:49:31 +0700

    update user controllers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-
+use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     //index
@@ -15,5 +15,31 @@ class UserController extends Controller
             ->orderBy('id', 'desc')
             ->paginate(10);
         return view('pages.users.index', compact('users'));
+    }
+
+    //create
+    public function create()
+    {
+        return view('pages.users.create');
+    }
+
+    //store
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'nim' => $request->nim,
+            'role' => $request->role,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return redirect()->route('users.index')->with('success', 'Data User berhasil dibuat');
     }
 }
