ab37e93e5ce9d5610d4a046d5e8022686d01abe3
danielpsrb <danielpsrb01@gmail.com>
2024-04-28 19:00:04 +0700

    update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+
+        $middleware->alias([
+
+            'admin' => \App\Http\Middleware\AdminAccess::class,
+
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
