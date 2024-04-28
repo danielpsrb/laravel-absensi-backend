4ecca82a61938d5b3eb7ffd09cee1d59899a4903
danielpsrb <danielpsrb01@gmail.com>
2024-04-28 23:24:42 +0700

    update app.php yaw

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\AdminUserMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,9 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
 
         $middleware->alias([
-
-            'admin' => \App\Http\Middleware\AdminAccess::class,
-
+            'admin' => AdminUserMiddleware::class,
         ]);
 
     })
