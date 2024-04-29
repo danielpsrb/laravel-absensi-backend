dba6a35fdbf41b5fa9cd0c059b5bccbdb96316f1
danielpsrb <danielpsrb01@gmail.com>
2024-04-29 21:07:34 +0700

    add auth controller for api login

---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    //login
+    public function login(Request $request)
+    {
+        $loginData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $loginData['email'])->first();
+
+        //check user exist
+        if (!$user) {
+            return response(['message' => 'Invalid credentials'], 401);
+        }
+
+        //check password
+        if (!Hash::check($loginData['password'], $user->password)) {
+            return response(['message' => 'Invalid credentials'], 401);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response(['user' => $user, 'token' => $token], 200);
+    }
+}
