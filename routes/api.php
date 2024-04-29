f91788b1c0bffc8abb49383c7ab68012015f15ec
danielpsrb <danielpsrb01@gmail.com>
2024-04-29 21:37:35 +0700

    update routes for api login

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//login
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+
