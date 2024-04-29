530a3f56119589bba3198e97336bafaac85bb4ff
danielpsrb <danielpsrb01@gmail.com>
2024-04-29 19:55:38 +0700

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,10 @@ class User extends Authenticatable
         'nim',
         'nip',
         'role',
+        'study_program',
+        'faculty',
+        'face_embedding',
+        'image_url',
     ];
 
     /**
