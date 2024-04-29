546dd5bbdb2609f83c97e9575126daf6bb018106
danielpsrb <danielpsrb01@gmail.com>
2024-04-29 21:02:30 +0700

    update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use App\Http\Middleware\AdminUserMiddleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
