a41cff2160970182b20344e64651147dafbc51c4
danielpsrb <danielpsrb01@gmail.com>
2024-05-05 21:22:17 +0700

    add models for attendance

---
+++ app/Models/Attendance.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'date',
+        'time_in',
+        'time_out',
+        'latlon_in',
+        'latlon_out',
+    ];
+}
