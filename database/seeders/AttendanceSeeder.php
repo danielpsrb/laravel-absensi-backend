818641bd19f08a2068d5be2fb3dbd3edb78dc5b8
danielpsrb <danielpsrb01@gmail.com>
2024-05-05 21:23:09 +0700

    add data dummy for attendance

---
+++ database/seeders/AttendanceSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Attendance;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AttendanceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Attendance::factory()->count(5)->create();
+    }
+}
