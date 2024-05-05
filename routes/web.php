2cc427af253f46b40a06b5027b0bfc4c1c71256e
danielpsrb <danielpsrb01@gmail.com>
2024-05-05 22:08:41 +0700

    update routes web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AttendanceController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\HomeController;
@@ -18,4 +19,5 @@ Route::group(['middleware' => 'admin'], function () {
 
     Route::resource('users', UserController::class);
     Route::resource('universites', UniversityController::class);
+    Route::resource('attendances', AttendanceController::class);
 });
