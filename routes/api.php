ed7d33d0225f6b78dea379f8a94da19255031bed
danielpsrb <danielpsrb01@gmail.com>
2024-05-05 13:55:55 +0700

    update routes api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\UniversitiesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,3 +15,5 @@ Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login'])
 //logout
 Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
 
+//university
+Route::get('/universities', [UniversitiesController::class, 'index'])->middleware('auth:sanctum');
