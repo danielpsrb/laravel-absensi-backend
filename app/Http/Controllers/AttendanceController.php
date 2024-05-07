e17959a055bc5ec4122d4a8f1663931045e1afac
danielpsrb <danielpsrb01@gmail.com>
2024-05-07 21:56:28 +0700

    fix: Attendance controller

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class AttendanceController extends Controller
 {
-    //index
+    //show data attendances
     public function index(Request $request)
     {
         $attendances = Attendance::with('user')
@@ -16,7 +16,29 @@ class AttendanceController extends Controller
                     $query->where('name', 'like', '%' . $name . '%');
                 });
             })->orderBy('id', 'desc')->paginate(10);
-        return view('pages.absensi.show', compact('attendances'));
+            return view('pages.absensi.show', compact('attendances'));
     }
 
+    //edit data attendances
+    public function edit($id)
+    {
+        $attendance = Attendance::find($id);
+        return view('pages.absensi.edit', compact('attendance'));
+    }
+
+    //update attendances
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'user_id' => 'required',
+            'date' => 'required',
+            'time_in' => 'required',
+            'time_out' => 'required',
+        ]);
+
+        $attendance = Attendance::find($id);
+        $attendance->update($request->all());
+
+        return redirect()->route('attendances.index')->with('success', 'Data Absensi berhasil diupdate');
+    }
 }
