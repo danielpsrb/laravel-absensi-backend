0c5df754bd1d4b8c818fea597d262ff0981e7f6a
danielpsrb <danielpsrb01@gmail.com>
2024-05-12 17:27:12 +0700

    chore:add endpoint for update profile on api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AttendanceController;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UniversitiesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -27,3 +28,6 @@ Route::post('/checkout', [AttendanceController::class, 'checkout'])->middleware(
 
 //is_checkin
 Route::get('/is-checkin', [AttendanceController::class, 'isCheckedin'])->middleware('auth:sanctum');
+
+//update profile
+Route::post('/update-profile', [AuthController::class, 'updateProfile'])->middleware('auth:sanctum');
