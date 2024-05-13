2a92af986767d371d72910c938873421ef56502e
danielpsrb <danielpsrb01@gmail.com>
2024-05-13 20:02:02 +0700

    update university seeder

--- database/seeders/UniversityDataSeeder.php
+++ database/seeders/UniversityDataSeeder.php
@@ -15,13 +15,15 @@ class UniversityDataSeeder extends Seeder
     {
         // Create a new university
         University::create([
-            'name' => 'University of Zimbabwe',
-            'email' => 'zimbabwe@example.com',
-            'address' => 'P.O. Box MP 167, Mount Pleasant, Harare, Zimbabwe',
-            'phone_number' => '263 242 303211',
-            'latitude' => -17.825165,
-            'longitude' => 31.033510,
-            'radius_km' => 1,
+            'name' => 'Sam Ratulangi University',
+            'email' => 'rektorat@unsrat.ac.id',
+            'address' => 'Kampus Unsrat Bahu, Malalayang, Manado City, North Sulawesi, Indonesia',
+            'phone_number' => '0431-863886',
+            'description' => 'Universitas Sam Ratulangi sering disingkat dengan sebutan UNSRAT adalah salah satu Perguruan Tinggi Negeri di Indonesia yang berlokasi di Kota Manado, Provinsi Sulawesi Utara[1]. Universitas Sam Ratulangi dipimpin oleh seorang Rektor Universitas Sam Ratulangi yang sekarang adalah Prof. Dr. Ir. Octovian Berty Alexander Sompie, M.Eng, IPU.',
+            'total_faculty' => 'Universitas Sam Ratulangi sebagai perguruan tinggi negeri memiliki jumlah 12  dengan Sebelas Fakultas dan satu Program Pasca Sarjana yaitu: Fakultas Kedokteran, Fakultas Teknik, Fakultas Pertanian, Fakultas Peternakan, Fakultas Perikanan dan Ilmu Kelautan, Fakultas Ekonomi, Fakultas Hukum, Fakultas Ilmu Sosial dan Ilmu Politik, Fakultas Sastra, Fakultas Matematika dan Ilmu Pengetahuan Alam, Fakultas Kesehatan Masyarakat, serta Fakultas Pasca Sarjana',
+            'latitude' => 1.455783,
+            'longitude' => 124.827215,
+            'radius_km' => 2,
             'time_in' => '08:00',
             'time_out' => '16:00',
         ]);
