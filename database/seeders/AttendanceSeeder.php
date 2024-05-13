da4bf132db46df2a4952fcb01cc8d430e9f4a801
danielpsrb <danielpsrb01@gmail.com>
2024-05-13 20:00:50 +0700

    update attendance seeder

--- database/seeders/AttendanceSeeder.php
+++ database/seeders/AttendanceSeeder.php
@@ -13,6 +13,6 @@ class AttendanceSeeder extends Seeder
      */
     public function run(): void
     {
-        Attendance::factory()->count(5)->create();
+        Attendance::factory()->count(9)->create();
     }
 }
