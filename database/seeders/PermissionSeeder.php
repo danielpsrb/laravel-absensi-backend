b5049a33d316edabe3b5d6d143e62abb2f0848ef
danielpsrb <danielpsrb01@gmail.com>
2024-05-13 20:05:11 +0700

    add seeder for data permission

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Permission::factory()->count(9)->create();
+    }
+}
