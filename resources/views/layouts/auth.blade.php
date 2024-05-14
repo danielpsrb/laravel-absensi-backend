a8f8b8e624ee4f4dbc92ccb15ab7c9fc761b18fe
danielpsrb <danielpsrb01@gmail.com>
2024-05-14 22:15:02 +0700

    update layout auth

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"
         name="viewport">
-    <title>@yield('title') &mdash; Stisla</title>
+    <title>@yield('title') &mdash; Admin</title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet"
@@ -48,12 +48,8 @@
                         class="{{ Request::is('auth-register') ? 'col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2' : 'col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4' }}">
                         <!-- Footer -->
                         @include('components.auth-header')
-
                         <!-- Content -->
                         @yield('main')
-
-                        <!-- Footer -->
-                        @include('components.auth-footer')
                     </div>
                 </div>
             </div>
