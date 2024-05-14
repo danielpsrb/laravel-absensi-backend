46d1ce6f106682d6ad8494591aec3963d28170d0
danielpsrb <danielpsrb01@gmail.com>
2024-05-14 22:17:25 +0700

    update header auth

--- resources/views/components/auth-header.blade.php
+++ resources/views/components/auth-header.blade.php
@@ -1,6 +1,7 @@
- <div class="login-brand">
-     <img src="{{ asset('img/stisla-fill.svg') }}"
-         alt="logo"
-         width="100"
-         class="shadow-light rounded-circle">
- </div>
+<div class="login-brand">
+    <img src="{{ asset('img/admin-logo.png')}}"
+        alt="logo"
+        width="100"
+        class="shadow-light rounded-circle bg-light"
+    >
+</div>
