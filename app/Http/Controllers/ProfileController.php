5aeb8eacbdcc2715fdd2d30bf703ffce9a19d44b
danielpsrb <danielpsrb01@gmail.com>
2024-05-19 22:20:49 +0700

    update profile controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,14 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
-    //feature-profile
-    public function profile()
+    //index
+    public function index()
     {
         return view('pages.profile.feature-profile');
+    }
+
+    //update-profile
+    public function update(Request $request)
+    {
+        $user = User::find(Auth::user()->id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+        return redirect()->route('profile')->with('success', 'Profile updated successfully');
+    }
+
+    public function destroy(Request $request)
+    {
+        $user = User::find(Auth::user()->id);
+        $user->delete();
+        return redirect()->route('login')->with('success', 'Profile deleted successfully');
     }
 
     //feature activities
