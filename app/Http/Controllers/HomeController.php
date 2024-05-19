415538db137e7eb4d54fe45e82c95268d3dfc01d
danielpsrb <danielpsrb01@gmail.com>
2024-05-19 22:22:37 +0700

    update home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,5 +10,11 @@ class HomeController extends Controller
     public function index()
     {
         return view('pages.dashboard', ['type_menu' => 'home']);
+    }
+
+    public function show()
+    {
+        $totalUsers = User::count();
+        return view('pages.dashboard', compact('totalUsers'));
     }
 }
