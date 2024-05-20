66d915f375cf2c006490a543afed4efb658f12e5
danielpsrb <danielpsrb01@gmail.com>
2024-05-20 17:19:32 +0700

    update pages for login

--- resources/views/pages/auth/auth-login.blade.php
+++ resources/views/pages/auth/auth-login.blade.php
@@ -1,104 +1,156 @@
-@extends('layouts.auth')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('title', 'Login')
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"
+        name="viewport">
+    <title>Login &mdash; Admin</title>
 
-@push('style')
+    <!-- General CSS Files -->
+    <link rel="stylesheet"
+        href="{{ asset('library/bootstrap/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer" />
+
     <!-- CSS Libraries -->
     <link rel="stylesheet"
         href="{{ asset('library/bootstrap-social/bootstrap-social.css') }}">
-@endpush
 
-@section('main')
-    <div class="card card-primary">
-        <div class="card-header">
-            <h4>Welcome, Admin Dashboard</h4>
-        </div>
+    <!-- Template CSS -->
+    <link rel="stylesheet"
+        href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('css/components.css') }}">
+</head>
 
-        <div class="card-body">
-            <form method="POST"
-                action="{{ route('login') }}"
-                class="needs-validation"
-                novalidate="">
-                @csrf
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                        value="{{ old('email') }}" name="email" tabindex="1" required>
-                    @error('email')
-                        <div class="invalid-feedback">
-                            {{ $message }}
+<body>
+    <div id="app">
+        <section class="section">
+            <div class="d-flex align-items-stretch flex-wrap">
+                <div class="col-lg-4 col-md-6 col-12 order-lg-1 min-vh-100 order-2 bg-white">
+                    <div class="m-3 p-4">
+                        <img src="{{ asset('img/admin-logo.png')}}"
+                            alt="logo Admin"
+                            width="80"
+                            class="shadow-light rounded-circle mb-5 mt-2">
+                        <h4 class="text-dark font-weight-normal">Welcome, <span class="font-weight-bold">Admin Dashboard</span>
+                        </h4>
+                        <p class="text-muted">Please login with credentialss or register if you don't already
+                            have an account.</p>
+                        <form method="POST"
+                            action="{{ route('login') }}"
+                            class="needs-validation"
+                            novalidate="">
+                            @csrf
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input id="email"
+                                    type="email"
+                                    class="form-control"
+                                    name="email"
+                                    tabindex="1"
+                                    required
+                                    autofocus>
+                                <div class="invalid-feedback">
+                                    Please fill in your email
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="d-block">
+                                    <label for="password"
+                                        class="control-label">Password</label>
+                                </div>
+                                <input id="password"
+                                    type="password"
+                                    class="form-control"
+                                    name="password"
+                                    tabindex="2"
+                                    required>
+                                <div class="invalid-feedback">
+                                    please fill in your password
+                                </div>
+                            </div>
+
+                            <div class="form-group mb-3 d-flex justify-content-between"">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox"
+                                        class="custom-control-input"
+                                        id="togglePasswordCheckbox"
+                                        tabindex="3">
+                                    <label class="custom-control-label"
+                                        for="togglePasswordCheckbox">Show Password</label>
+                                </div>
+                                <div>
+                                    <a href="{{ route('password.request') }}"
+                                        class="text-small">
+                                        Forgot Password?
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="form-group text-left">
+                                <button type="submit"
+                                    class="btn btn-primary btn-lg btn-icon icon-right"
+                                    tabindex="4">
+                                    Login
+                                </button>
+                            </div>
+
+                            <div class="mt-5 text-center">
+                                Don't have an account? <a href="{{route('register')}}">Signup now</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-8 col-12 order-lg-2 min-vh-100 background-walk-y position-relative overlay-gradient-bottom order-1"
+                    data-background="https://images.unsplash.com/photo-1605942971894-a996c47ca995?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFuYWRvfGVufDB8fDB8fHww">
+                    <div class="absolute-bottom-left index-2">
+                        <div class="text-light p-5 pb-2">
+                            <div class="mb-5 pb-3">
+                                <h1 class="display-4 font-weight-bold mb-2">Hello</h1>
+                                <h5 class="font-weight-normal text-muted-transparent">Manado, Indonesia</h5>
+                            </div>
                         </div>
-                    @enderror
-                    <div class="invalid-feedback">
-                        Please Fill in your Email
                     </div>
                 </div>
-
-                <div class="form-group">
-                    <div class="d-block">
-                        <label for="password"
-                            class="control-label">
-                            Password
-                        </label>
-
-                    </div>
-                    <div class="input-group position-relative">
-                        <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
-                        <span class="input-group-text">
-                            <img src="{{ asset('img/eye.png') }}" width="20" id="togglePassword" class="password-toggle" style="cursor: pointer" onclick="togglePasswordVisibility()">
-                        </span>
-                        <div class="invalid-feedback">
-                            Please Fill in your Password
-                        </div>
-                    </div>
-                </div>
-
-                <div class="form-group mb-3 d-flex justify-content-between"">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox"
-                            name="remember"
-                            class="custom-control-input"
-                            tabindex="3"
-                            id="remember-me">
-                        <label class="custom-control-label"
-                            for="remember-me">Remember Me</label>
-                    </div>
-                    <div>
-                        <a href="{{ route('password.request') }}"
-                            class="text-small">
-                            Forgot Password?
-                            </a>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <button type="submit"
-                        class="btn btn-primary btn-lg btn-block"
-                        tabindex="4">
-                        Login
-                    </button>
-                </div>
-            </form>
-        </div>
-        <div class="text-muted mt-15 text-center">
-            Don't have an account ? <a href="{{route('register')}}">Signup Now</a>
-        </div>
+            </div>
+        </section>
     </div>
-@endsection
 
-@push('scripts')
+    <!-- General JS Scripts -->
+    <script src="{{ asset('library/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('library/popper.js/dist/umd/popper.js') }}"></script>
+    <script src="{{ asset('library/tooltip.js/dist/umd/tooltip.js') }}"></script>
+    <script src="{{ asset('library/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('library/jquery.nicescroll/dist/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ asset('library/moment/min/moment.min.js') }}"></script>
+    <script src="{{ asset('js/stisla.js') }}"></script>
+
+    <!-- JS Libraies -->
+
+    <!-- Page Specific JS File -->
+
+    <!-- Template JS File -->
+    <script src="{{ asset('js/scripts.js') }}"></script>
+    <script src="{{ asset('js/custom.js') }}"></script>
     <script>
-        function togglePasswordVisibility() {
-            var passwordField = document.getElementById("password");
-            var toggleButton = document.getElementById("togglePassword");
-
-            if (passwordField.type === "password") {
-                passwordField.type = "text";
-                toggleButton.src = "{{ asset('img/eye-hide.png') }}"; // Mengubah gambar menjadi ikon mata tersembunyi
+        document.getElementById('togglePasswordCheckbox').addEventListener('change', function () {
+            const passwordField = document.getElementById('password');
+            const passwordLabel = document.querySelector('label[for="togglePasswordCheckbox"]');
+            if (this.checked) {
+                passwordField.setAttribute('type', 'text');
+                passwordLabel.textContent = 'Hide Password';
             } else {
-                passwordField.type = "password";
-                toggleButton.src = "{{ asset('img/eye.png') }}"; // Mengubah gambar kembali menjadi ikon mata biasa
+                passwordField.setAttribute('type', 'password');
+                passwordLabel.textContent = 'Show Password';
             }
-        }
+        });
     </script>
-@endpush
+</body>
+
+</html>
