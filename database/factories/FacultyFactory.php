d3e2e5feeaee21dd426b814599eff68873d88e62
danielpsrb <danielpsrb01@gmail.com>
2024-05-24 21:56:51 +0700

    add factories for department and faculty

---
+++ database/factories/FacultyFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Faculty>
+ */
+class FacultyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'name' => $this->faker->name,
+            'description' => $this->faker->sentence,
+            'dean' => $this->faker->name,
+        ];
+    }
+}
