a45352f3f81e426acf368dec3324ce847132fe52
danielpsrb <danielpsrb01@gmail.com>
2024-05-24 21:58:43 +0700

    update config session for login

--- config/session.php
+++ config/session.php
@@ -32,7 +32,7 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 120),
+    'lifetime' => env('SESSION_LIFETIME', 10),
 
     'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
 
