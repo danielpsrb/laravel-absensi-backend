544b73d9451655e7deb98d583d1117b0158089e5
danielpsrb <danielpsrb01@gmail.com>
2024-06-09 00:23:13 +0700

    feat: add full calender in dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,18 +3,44 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('pages.dashboard', ['type_menu' => 'home']);
+        $datas = array();
+        $events = Event::all();
+        foreach ($events as $event) {
+            $datas[] = [
+                'id' => $event->id,
+                'title' => $event->title,
+                'start' => $event->start_date,
+                'end' => $event->end_date,
+            ];
+        }
+
+        return view('pages.dashboard', [
+            'type_menu' => 'home',
+            'datas' => $datas,
+        ]);
     }
 
     public function show()
     {
         $totalMahasiswa = User::where('role', 'user')->count();
-        return view('pages.dashboard', compact('totalMahasiswa'));
+        $datas = array();
+        $events = Event::all();
+        foreach ($events as $event) {
+            $datas[] = [
+                'id' => $event->id,
+                'title' => $event->title,
+                'start' => $event->start_date,
+                'end' => $event->end_date,
+            ];
+        }
+
+        return view('pages.dashboard', compact('totalMahasiswa', 'datas'));
     }
 }
