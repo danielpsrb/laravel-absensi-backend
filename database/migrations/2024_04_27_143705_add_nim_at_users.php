36cb621b3cfcbcf00f8aa7da7c0a3a9ebd1ea92f
danielpsrb <danielpsrb01@gmail.com>
2024-06-09 11:04:02 +0700

    change the nim and nip data types

--- database/migrations/2024_04_27_143705_add_nim_at_users.php
+++ database/migrations/2024_04_27_143705_add_nim_at_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('nim')->nullable();
+            $table->char('nim', 12)->nullable();
         });
     }
 
