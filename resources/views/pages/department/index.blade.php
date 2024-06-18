56fa580fa1855979eb83251e9a796412ff996238
danielpsrb <danielpsrb01@gmail.com>
2024-06-19 00:43:32 +0700

    update pages study program

--- resources/views/pages/department/index.blade.php
+++ resources/views/pages/department/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Permissions')
+@section('title', 'Study Programs')
 
 @push('style')
     <!-- CSS Libraries -->
@@ -11,14 +11,14 @@
     <div class="main-content">
         <section class="section">
             <div class="section-header">
-                <h1>Permissions</h1>
+                <h1>Study Program</h1>
                 {{-- <div class="section-header-button">
                     <a href="{{ route('permissions.create') }}" class="btn btn-primary">Add New</a>
                 </div> --}}
                 <div class="section-header-breadcrumb">
-                    <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-                    <div class="breadcrumb-item"><a href="#">Study Program</a></div>
-                    <div class="breadcrumb-item">All Study Program</div>
+                    <div class="breadcrumb-item active"><a href="#"><i class="fas fa-home"></i> Home</a></div>
+                    <div class="breadcrumb-item"><a href="#"><i class="fas fa-graduation-cap"></i> Study Program</a></div>
+                    <div class="breadcrumb-item"><i class="fas fa-list"></i> Data</div>
                 </div>
             </div>
             <div class="section-body">
